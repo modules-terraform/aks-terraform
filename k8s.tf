ef9808e0fe9ed302a6772c2e30f7f9b57ae97f2d
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-02-24 14:10:31 +0000

    - Add Rbac to cluster

--- k8s.tf
+++ k8s.tf
@@ -34,6 +34,10 @@ resource "azurerm_kubernetes_cluster" "k8s" {
         }
     }
 
+    role_based_access_control {
+        enabled = true
+    }
+
     tags = {
         Environment = "Development"
     }

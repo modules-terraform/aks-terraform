69300a9f622b64fb20c7f2bab855c197669be582
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-03-01 15:01:26 -0300

    - Fixed the interpolation in azure ad app

--- azuread-application.tf
+++ azuread-application.tf
@@ -6,11 +6,11 @@ resource "azuread_application" "aksadapp" {
 }
 
 resource "azuread_service_principal" "aksadsp" {
-  application_id = azuread_application.aksadapp.application_id
+  application_id              = azuread_application.aksadapp.application_id
 }
 
 resource "azuread_service_principal_password" "example" {
-  service_principal_id = "${azuread_service_principal.aksadsp.id}"
-  value                = "VT=uSgbTanZhyz@%nL9Hpd+Tfay_MRV#"
-  end_date             = "2020-12-31T23:59:59Z"
+  service_principal_id        = azuread_service_principal.aksadsp.id
+  value                       = "VT=uSgbTanZhyz@%nL9Hpd+Tfay_MRV#"
+  end_date                    = "2020-12-31T23:59:59Z"
 }

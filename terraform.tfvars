f7c0deedffec6d86e4453bd5acd626b49bba39d8
Fábio Henrique Gabriele <Fabioh@users.noreply.github.com>
2020-03-29 14:08:21 -0300

    add-tfvars-sample-file (#3)
    * - Add types to variables files
    
    * - Add terraform.tfvars sample file
    
    * - Remove interpolation from resource-group.tf file
    
    * - Add variable to specify kubernetes version

---
+++ terraform.tfvars
@@ -0,0 +1,15 @@
+agent_count = 2
+
+kubernetes_version = "1.16.7"
+
+ssh_public_key = "./keys/azure_key.pub"
+
+dns_prefix = "aksplayground"
+
+cluster_name = "aksplayground"
+
+resource_group_name = "aksplayground-tf-group"
+
+location = "east us"
+
+log_analytics_workspace_location = "east us"
--- variables.tf
+++ variables.tf
@@ -1,41 +1,55 @@
 variable "agent_count" {
-    default = 3
+  default = 3
+  type = number
+}
+
+variable "kubernetes_version" {
+  type = string
 }
 
 variable "ssh_public_key" {
-    default = "./keys/azure_key.pub"
+  default = "./keys/azure_key.pub"
+  type = string
 }
 
 variable "dns_prefix" {
-    default = "aksplayground"
+  default = "aksplayground"
+  type = string
 }
 
 variable cluster_name {
-    default = "aksplayground"
+  default = "aksplayground"
+  type = string
 }
 
 variable resource_group_name {
-    default = "aksplayground-tf-group"
+  default = "aksplayground-tf-group"
+  type = string
 }
 
 variable "resource_group_name_data_analytics" {
   default = "aksplayground-tf-group-data-analytics-group"
+  type = string
 }
 
 variable location {
-    default = "brazil south"
+  default = "brazil south"
+  type = string
 }
 
 variable log_analytics_workspace_name {
-    default = "testLogAnalyticsWorkspaceName"
+  default = "testLogAnalyticsWorkspaceName"
+  type = string
 }
 
 # refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
 variable log_analytics_workspace_location {
-    default = "brazil south"
+  default = "brazil south"
+  type = string
 }
 
 # refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
 variable log_analytics_workspace_sku {
-    default = "PerGB2018"
+  default = "PerGB2018"
+  type = string
 }

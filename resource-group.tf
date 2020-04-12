3a8f195a0d00f0b1c8f8f7262fe54823bb165dc3
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-04-12 10:36:34 -0300

    - Creation of the resource group of the log analytcs based in variable use_azure_monitor;

--- resource-group.tf
+++ resource-group.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "k8s" {
 }
 
 resource "azurerm_resource_group" "log_analytics" {
+    count               = var.use_azure_monitor ? 1 : 0
     name     = var.resource_group_name_data_analytics
     location = var.location
 }

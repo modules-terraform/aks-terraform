c3a10caff621529d7ae3fdbdf937e8bb5fc2bdf4
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-05-02 21:52:51 -0300

    Separate 'azurerm_log_analytics_solution' in a file

--- modules/log-analytics/log-analytics-workspace.tf
+++ modules/log-analytics/log-analytics-workspace.tf
@@ -23,17 +23,3 @@ resource "azurerm_log_analytics_workspace" "akslogsworkspace" {
     var.custom_tags
   )
 }
-
-resource "azurerm_log_analytics_solution" "akslogssolution" {
-  count                 = var.use_azure_monitor ? 1 : 0
-  solution_name         = "ContainerInsights"
-  location              = azurerm_log_analytics_workspace.akslogsworkspace[count.index].location
-  resource_group_name   = azurerm_resource_group.log_analytics[count.index].name
-  workspace_resource_id = azurerm_log_analytics_workspace.akslogsworkspace[count.index].id
-  workspace_name        = azurerm_log_analytics_workspace.akslogsworkspace[count.index].name
-
-  plan {
-    publisher = "Microsoft"
-    product   = "OMSGallery/ContainerInsights"
-  }
-}

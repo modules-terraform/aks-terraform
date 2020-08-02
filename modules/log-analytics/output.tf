0fc0631b95fa397686faa7ba7d010c83d7d57c0b
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-08-02 20:28:35 -0300

    - Fix output error in case of variable 'use_azure_monitor' is false

--- modules/log-analytics/output.tf
+++ modules/log-analytics/output.tf
@@ -1,3 +1,4 @@
+# https://github.com/hashicorp/terraform/issues/23222#issuecomment-547399016
 output "workspace-id"{
-    value = azurerm_log_analytics_workspace.akslogsworkspace[0].id
+    value =  length(azurerm_log_analytics_workspace.akslogsworkspace) > 0 ? azurerm_log_analytics_workspace.akslogsworkspace[0].id : ""
 }
